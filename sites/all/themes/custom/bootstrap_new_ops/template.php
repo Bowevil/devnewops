0e4416eca220a8548f4dbfba4a6e3c30b13283d5
Michael Tripp <ixtlandes@mindspring.com>
2014-07-14 17:32:21 -0400

    implemented appletouch icons and corrected path to favicon

--- sites/all/themes/custom/bootstrap_new_ops/template.php
+++ sites/all/themes/custom/bootstrap_new_ops/template.php
@@ -4,3 +4,33 @@
  * @file
  * template.php
  */
+function new_ops_preprocess_html(&$variables) {
+  /**
+   * Add default icon
+   */
+  $apple_icon =  array(
+
+    '#tag' => 'link',
+    '#attributes' => array(
+      'href' => drupal_get_path('theme',$GLOBALS['theme']),'/apple-touch-icon.png',
+      'rel' => 'apple-touch-icon-precomposed',
+    ),
+  );
+
+  /**
+   * Loop through to add various sizes
+   */
+  $apple_icon_sizes = array(76,120,152);
+
+  foreach($apple_icon_sizes as $size){
+    $apple = array(
+      '#tag' => 'link',
+      '#attributes' => array(
+        'href' => drupal_get_path('theme',$GLOBALS['theme']).'/apple-touch-icon-'.$size.'px.png',
+        'rel' => 'apple-touch-icon-precomposed',
+        'sizes' => $size . 'x' . $size,
+      ),
+    );
+    drupal_add_html_head($apple, 'apple-touch-icon-'.$size);
+  }
+}
